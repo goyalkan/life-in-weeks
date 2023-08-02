a54abd72f1aa4f32e33f7df23f608d7768ba2699
Kanishka Goyal <127941423+goyalkan@users.noreply.github.com>
2023-08-02 20:45:16 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+age = input("What is your current age? ")
+
+Age = int(age)
+
+a = str( (90 - Age)*365 )
+b = str( ( 90 - Age)*52)
+c = str( (90 - Age) *12)
+print("you have " + a + " days,  " + b + " weeks, " + "and " + c + " months left." )
+
